1793130dae01dec5e7aa22072faace35cb47a4f8
glodie <glodie.tshimini@gmail.com>
2024-01-24 14:44:34 +0100

    feat: help handle exercise 1

---
+++ demo/js/ex1.js
@@ -0,0 +1,31 @@
+// Attente organisation de l'exercice 1
+
+/**
+ * Partie déclaration
+ *  1 algo = une fonction
+ * 
+ * */ 
+
+function getAge(age) {
+    console.log('age ', age)
+}
+
+function avg() {
+    console.log('avg')
+}
+
+function vat () {
+    console.log('vat')
+}
+
+/**
+ * Partie exécution
+ *  soit côté navigateur avec un HTML et affichage des informations dans la devTools (F12) avec la console
+ *  ou avec Node directement depuis un terminal en écrivant la commande node chemin_relatif_vers_le_fichier.js
+*/
+console.log('getAge 18', getAge(18))
+getAge(45)
+getAge(85)
+avg()
+vat()
+
