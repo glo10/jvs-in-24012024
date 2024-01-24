5eb6e63c62bd30252b10226e65b148620700491d
glodie <glodie.tshimini@gmail.com>
2024-01-24 11:33:31 +0100

    feat: start array

---
+++ demo/table.js
@@ -0,0 +1,13 @@
+/**
+ * Une variable qui peut stocker plusieurs valeurs
+ * la première valeur est placée à l'indice 0
+ * déclaration d'un tableau avec []
+ */
+
+const numbers = []
+
+numbers[0] = 1 // au rang ou à l'indice 0 on affecte la valeur 4
+numbers[1] = 10
+numbers[10] = 100
+
+console.log('tableau nombre', numbers, 'taille du tableau', numbers.length)
