a06ae851d9bece8fc641ef7221678795ff0714c9
glodie <glodie.tshimini@gmail.com>
2024-01-24 14:59:36 +0100

    refactor: explanation do while

--- demo/js/boucles.js
+++ demo/js/boucles.js
@@ -18,9 +18,20 @@ for(let city of cities) {
 // Avec la boucle while
 let age = 0
 do {
-    age = prompt('Saisir un age supérieur à 18 ans')
+    age = prompt('Saisir un age supérieur ou égale à 18 ans')
     age = parseInt(age)
-    console.log('age', age )
+    console.error(age, 'pas >= 18' )
+    /**
+     * Avec do même si on saisit un age correctement étant donnée que la condition est vérifié à la fin
+     * l'age correcte sera d'abord affiché (do) puis après la condition sera fausse
+     * et on sortira de la boucle
+     * Pour ne pas afficher le console.error, il faudra mettre une condition pour dire
+     * qu'on affiche uniquement quand l'age est inférieur à 18
+     */
 } while(age < 18)
 // Avec la boucle 
-console.log('sortien de la boucle')
+console.log(
+    'tant que l\'age n\'est pas correcte',
+    'l\'utilisateur reste coincé dans la boucle do while',
+    'et cette ligne ne s\'affiche que lorsqu\'il en sort'
+)
