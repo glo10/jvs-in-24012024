3681195b5cf757897f0255c4764d78484651514a
glodie <glodie.tshimini@gmail.com>
2024-01-25 10:41:09 +0100

    refactor: update demo createElement

--- demo/js/dom-selector.js
+++ demo/js/dom-selector.js
@@ -4,3 +4,10 @@ const h2 = document.querySelector('h2')
 console.log('h2', h2)
 const items = document.querySelectorAll('li')
 console.dir('li', items)
+// séléctionner ul
+const ul = document.querySelector('ul')
+// créer un li avec la valeur textuelle 10
+const item10 = document.createElement('li')
+item10.innerText = 'nouvelle créé avec JS 10'
+// Ajouter ce li à ul
+ul.append(item10)
