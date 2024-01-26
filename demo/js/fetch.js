dd3580f078c2bcfb825f5e2e6da06190a4c8ce3a
glodie <glodie.tshimini@gmail.com>
2024-01-26 14:30:14 +0100

    refactor: todolist

--- demo/js/fetch.js
+++ demo/js/fetch.js
@@ -22,17 +22,42 @@ fetch('https://api.open-meteo.com/v1/forecast?latitude=48.896676&longitude=2.229
  * Template HTML
  */
 
-fetch('http://127.0.0.1:7000/https://m2iformation.fr')
-.then(res => res.text())
-.then(res => console.log('m2i', res))
-.catch(error => console.error('erreur m2i html', error))
+// fetch('http://127.0.0.1:7000/https://m2iformation.fr')
+// .then(res => res.text())
+// .then(res => console.log('m2i', res))
+// .catch(error => console.error('erreur m2i html', error))
 
 /**
  * TODOLIST à partir d'une API
  *  https://jsonplaceholder.typicode.com/todos
  * 
  */
-
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(data => data.json())
+.then(todos => {
+    console.log('todos', todos)
+    for(let i = 0; i < 9; i++) {
+        const todo = todos[i]
+        let input = ''
+        if(todo.completed) {
+            input = `
+            <div>
+            <input type="checkbox" id="scales" name="scales" checked/>
+            <label for="scales">${todo.title}</label>
+            </div>
+            `
+        } else {
+            input = `
+            <div>
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">${todo.title}</label>
+            </div>
+            `
+        }
+        document.body.insertAdjacentHTML('afterbegin', input)
+    }
+})
+.catch(error => console.error('erreur m2i html', error))
 /**
  * Article news
  *  https://www.mac4ever.com/flux/json/content/all
